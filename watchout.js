e18700311d42715b1e4d96767d297e14e8a80054
Michael Harris <mwharris333@gmail.com>
2015-04-04 13:32:36 -0700

    Added transitions, updates x and y of enemy instances

--- watchout.js
+++ watchout.js
@@ -15,6 +15,10 @@ svg.append('rect')
   .attr('height', g.screen.height)
   .attr('fill', g.screen.bgColor);
 
+var randomLoc = function(px){
+  return Math.floor(Math.random() * px);
+}
+
 var Unit = function(x, y, width, height, image){
   this.x = x || 1;
   this.y = y ||  1;
@@ -26,26 +30,27 @@ var Unit = function(x, y, width, height, image){
   this.r = this.width/2;
 }
 
+var Player = function(x, y, width, height, image){
+  Unit.call(this, x, y, width, height, 'player.png');
+  this.class = 'player';
+}
+window.player = [new Player(randomLoc(g.screen.width),randomLoc(g.screen.height),10,10,'player.png')];
+
 var Enemy = function(x, y, width, height, image){
   Unit.call(this, x, y, width, height, image);
+  this.class = 'enemy';
 }
 
 window.enemies = [];
 var createEnemies = function(n){
   for (var i=0; i<n; i++){
-    window.enemies.push(new Enemy(i*40,i*40, 20,20,'asteroid.png'));
+    window.enemies.push(new Enemy(randomLoc(g.screen.width),randomLoc(g.screen.height), 20,20,'asteroid.png'));
   }
 }
-
 createEnemies(4);
 
-var randomLoc = function(px){
-  return Math.floor(Math.random() * px);
-}
-
-//var text = svg.append('circle').data(data, function(d) { return d; });
-var d3Enemies = svg.selectAll('image').data(window.enemies)
-
+// create enemies
+var d3Enemies = svg.selectAll('image').data(window.enemies);
 d3Enemies.enter().append('svg:image')
   .attr('class','enemy')
   .attr('x', function(d){return d.x})
@@ -53,3 +58,32 @@ d3Enemies.enter().append('svg:image')
   .attr('width', function(d){return d.width})
   .attr('height', function(d){return d.height})
   .attr("xlink:href", function(d){return d.image});
+
+// create player
+var d3Player = svg.append('svg:image').data(window.player);
+d3Player
+  .attr('class','player')
+  .attr('x', function(d){return d.x})
+  .attr('y', function(d){return d.y})
+  .attr('width', function(d){return d.width})
+  .attr('height', function(d){return d.height})
+  .attr("xlink:href", function(d){return d.image});
+
+d3Enemies.each(function(d){
+  log(d.class, d.image);
+});
+
+d3Enemies.each(function(d){
+  log(d.class, d.image);
+});
+
+var repeat = function() {
+  d3Enemies
+  .transition()
+  .duration(1000)
+  .attr('x',function(d){d.x = randomLoc(g.screen.width); return d.x})
+  .attr('y',function(d){d.y = randomLoc(g.screen.height); return d.y})
+  .each("end", repeat);
+}
+
+repeat();
