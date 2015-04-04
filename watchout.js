20735be08fb90267cdb4d29831e37ba793b3edfa
Michael Harris <mwharris333@gmail.com>
2015-04-04 12:16:00 -0700

    Added enemies rendering as images and has 'enemy' class attribute

--- watchout.js
+++ watchout.js
@@ -15,26 +15,41 @@ svg.append('rect')
   .attr('height', g.screen.height)
   .attr('fill', g.screen.bgColor);
 
-var Unit = function(cx, cy, r, fill){
-  this.cx = cx || 50;
-  this.cy = cy ||  50;
-  this.r = r ||  10;
-  this.fill = fill || '#fff';
+var Unit = function(x, y, width, height, image){
+  this.x = x || 1;
+  this.y = y ||  1;
+  this.width = width ||  10;
+  this.height = height || 10;
+  this.image = image || 'asteroid.png';
+
+  this.center = {'x':this.width/2 ,'y':this.height/2}
+  this.r = this.width/2;
 }
 
-var Enemy = function(cx, cy, r, fill){
-  Unit.call(this, cx, cy, 15, '#f00');
+var Enemy = function(x, y, width, height, image){
+  Unit.call(this, x, y, width, height, image);
 }
 
 window.enemies = [];
 var createEnemies = function(n){
   for (var i=0; i<n; i++){
-    window.enemies.push(new Enemy(n*10,n*10));
-  }  
+    window.enemies.push(new Enemy(i*40,i*40, 20,20,'asteroid.png'));
+  }
 }
 
-createEnemies(10);
+createEnemies(4);
 
 var randomLoc = function(px){
   return Math.floor(Math.random() * px);
 }
+
+//var text = svg.append('circle').data(data, function(d) { return d; });
+var d3Enemies = svg.selectAll('image').data(window.enemies)
+
+d3Enemies.enter().append('svg:image')
+  .attr('class','enemy')
+  .attr('x', function(d){return d.x})
+  .attr('y', function(d){return d.y})
+  .attr('width', function(d){return d.width})
+  .attr('height', function(d){return d.height})
+  .attr("xlink:href", function(d){return d.image});
